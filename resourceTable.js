188bc75edd4cc655d8783bf19048a6b19b5d1e8c
Michael Mrowetz <me@michaelmrowetz.com>
2014-10-05 21:04:41 -0400

    basic table output

---
+++ resourceTable.js
@@ -0,0 +1,29 @@
+var allRessources = performance.getEntriesByType("resource");
+
+
+var allRessourcesCalc = allRessources.map(function(currR, i, arr){
+
+	var urlFragments = currR.name.match(/:\/\/(.[^/]+)([^?]*)\??(.*)/);
+	var maybeFileName = urlFragments[2].split("/").pop();
+	var urlFragments = currR.name.match(/:\/\/(.[^/]+)([^?]*)\??(.*)/);
+
+	var currRes = {
+		name : currR.name,
+		domain : urlFragments[1],
+		fileExtension : maybeFileName.substr((Math.max(0, maybeFileName.lastIndexOf(".")) || Infinity) + 1),
+		loadtime : currR.duration,
+	};
+
+	if (currR.requestStart) {
+		currRes.dns = currR.domainLookupEnd - currR.domainLookupStart;
+		currRes.tcp = currR.connectEnd - currR.connectStart;
+		currRes.ttfb = currR.responseStart - currR.startTime;
+	}
+	if (currR.secureConnectionStart) {
+		currRes.ssl = currR.connectEnd - currR.secureConnectionStart;
+	}
+	console.log(currRes);
+	return currRes;
+});
+
+console.table(allRessourcesCalc);
